b18448c488b28c7b147441d14ba6c774e771138b
Andrew <osx@Osxs-MacBook-Pro.local>
2019-12-04 21:05:11 +0200

    some fixes

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -2,99 +2,105 @@ import React, { Component } from 'react'
 import './home.css'
 
 export class home extends Component {
-  render() {
-    return (
-      <div>
-        <body
-          data-spy="scroll"
-          data-target=".main-navigation"
-          data-offset="150"
-        >
-          <section id="MainContainer">
-            <header id="Header">
-              <nav class="main-navigation">
-                <div class="container clearfix">
-                  <div class="container clearfix">
-                    <div class="site-logo-wrap">
-                      <a class="logo" href="#"></a>
-                    </div>
-                    <a
-                      href="javascript:void(0)"
-                      class="menu-trigger hidden-lg-up"
-                    >
-                      <span>&nbsp;</span>
-                    </a>
-                    <div class="main-menu hidden-md-down">
-                      <ul class="menu-list">
-                        <li>
-                          <a
-                            class="nav-link"
-                            href="javascript:void(0)"
-                            data-target="#HeroBanner"
-                          >
-                            Home
-                          </a>
-                        </li>
+	render() {
+		return (
+			<div>
+				<body
+					data-spy='scroll'
+					data-target='.main-navigation'
+					data-offset='150'
+				>
+					<section id='MainContainer'>
+						<header id='Header'>
+							<nav class='main-navigation'>
+								<div class='container clearfix'>
+									<div class='container clearfix'>
+										<div class='site-logo-wrap'>
+											<a class='logo' href='#'></a>
+										</div>
+										<a
+											href='javascript:void(0)'
+											class='menu-trigger hidden-lg-up'
+										>
+											<span>&nbsp;</span>
+										</a>
+										<div class='main-menu hidden-md-down'>
+											<ul class='menu-list'>
+												<li>
+													<a
+														class='nav-link'
+														href='javascript:void(0)'
+														data-target='#HeroBanner'
+													>
+														Home
+													</a>
+												</li>
 
-                        <li>
-                          <a
-                            class="nav-link"
-                            href="javascript:void(0)"
-                            data-target="#ContactUs"
-                          >
-                            Contact
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
+												<li>
+													<a
+														class='nav-link'
+														href='javascript:void(0)'
+														data-target='#ContactUs'
+													>
+														Contact
+													</a>
+												</li>
+											</ul>
+										</div>
+									</div>
 
-                  <div class="mobile-menu hidden-lg-up">
-                    <ul class="mobile-menu-list">
-                      <li>
-                        <a
-                          class="nav-link"
-                          href="javascript:void(0)"
-                          data-target="#HeroBanner"
-                        >
-                          Home
-                        </a>
-                      </li>
+									<div class='mobile-menu hidden-lg-up'>
+										<ul class='mobile-menu-list'>
+											<li>
+												<a
+													class='nav-link'
+													href='javascript:void(0)'
+													data-target='#HeroBanner'
+												>
+													Home
+												</a>
+											</li>
 
-                      <li>
-                        <a
-                          class="nav-link"
-                          href="javascript:void(0)"
-                          data-target="#ContactUs"
-                        >
-                          Contact
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </nav>
-            </header>
-            <section id="HeroBanner">
-              <div class="hero-content">
-                <h1>Welcome to GUC parking spots</h1>
-                <p>Reserver your parking spot</p>
-                <a href="/spots" class="hero-cta">
-                  C6
-                </a>
-                <a href="/spots" class="hero-cta">
-                  C7
-                </a>
-                <a href="/spots" class="hero-cta">
-                  C8
-                </a>
-              </div>
-            </section>
-          </section>
-        </body>
-      </div>
-    )
-  }
+											<li>
+												<a
+													class='nav-link'
+													href='javascript:void(0)'
+													data-target='#ContactUs'
+												>
+													Contact
+												</a>
+											</li>
+										</ul>
+									</div>
+								</div>
+							</nav>
+						</header>
+						<section id='HeroBanner'>
+							<div class='hero-content'>
+								<h1>Welcome to GUC parking spots</h1>
+								<p>Reserve your parking spot</p>
+								<a href='/spots' class='hero-cta'>
+									P1
+								</a>
+								<a href='/spots' class='hero-cta'>
+									P5
+								</a>
+								<a href='/spots' class='hero-cta'>
+									P6
+								</a>
+								<a href='/spots' class='hero-cta'>
+									P7
+								</a>
+								<a href='/spots' class='hero-cta'>
+									P8
+								</a>
+							</div>
+						</section>
+					</section>
+				</body>
+			</div>
+		)
+	}
 }
 
 export default home
--- client/src/components/spots/spots.js
+++ client/src/components/spots/spots.js
@@ -11,114 +11,114 @@ import FormGroup from '@material-ui/core/FormGroup'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import Switch from '@material-ui/core/Switch'
 const useStyles = makeStyles(theme => ({
-  root: {
-    width: '100%'
-  },
-  heading: {
-    fontSize: theme.typography.pxToRem(15),
-    flexBasis: '33.33%',
-    flexShrink: 0
-  },
-  secondaryHeading: {
-    fontSize: theme.typography.pxToRem(15),
-    color: theme.palette.text.secondary
-  }
+	root: {
+		width: '100%'
+	},
+	heading: {
+		fontSize: theme.typography.pxToRem(15),
+		flexBasis: '33.33%',
+		flexShrink: 0
+	},
+	secondaryHeading: {
+		fontSize: theme.typography.pxToRem(15),
+		color: theme.palette.text.secondary
+	}
 }))
 
 export class spots extends Component {
-  state = {
-    expanded: null,
-    longitude: '',
-    latitude: '',
-    checkedA: true
-  }
+	state = {
+		expanded: null,
+		longitude: '',
+		latitude: '',
+		checkedA: true
+	}
 
-  componentDidMount() {
-    fetch('/api/spots/allSpots').then(async res => {
-      console.log(await res.json())
-    })
-  }
+	componentDidMount() {
+		fetch('/api/spots/allSpots').then(async res => {
+			console.log(await res.json())
+		})
+	}
 
-  handleChange = panel => (event, expanded) => {
-    this.setState({
-      expanded: expanded ? panel : false
-    })
-  }
+	handleChange = panel => (event, expanded) => {
+		this.setState({
+			expanded: expanded ? panel : false
+		})
+	}
 
-  handleChange2 = name => event => {
-    this.setState({ [name]: event.target.checked })
-  }
+	handleChange2 = name => event => {
+		this.setState({ [name]: event.target.checked })
+	}
 
-  render() {
-    const { classes } = this.props
-    const { expanded } = this.state
+	render() {
+		const { classes } = this.props
+		const { expanded } = this.state
 
-    return (
-      <div>
-        <FormGroup row>
-          <FormControlLabel
-            control={
-              <Switch
-                checked={this.state.checkedB}
-                onChange={this.handleChange2('checkedB')}
-                value="checkedB"
-                color="primary"
-              />
-            }
-            label="Empty spaaces"
-          />
-        </FormGroup>
+		return (
+			<div>
+				<FormGroup row>
+					<FormControlLabel
+						control={
+							<Switch
+								checked={this.state.checkedB}
+								onChange={this.handleChange2('checkedB')}
+								value='checkedB'
+								color='primary'
+							/>
+						}
+						label='Empty spaces'
+					/>
+				</FormGroup>
 
-        <div className={classes.root}>
-          <ExpansionPanel
-            expanded={expanded === 'panel1'}
-            onChange={this.handleChange('panel1')}
-          >
-            <ExpansionPanelSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel1bh-content"
-              id="panel1bh-header"
-            >
-              <Typography className={classes.heading}>
-                General settings
-              </Typography>
-              <Typography className={classes.secondaryHeading}>
-                I am an expansion panel
-              </Typography>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-              <Typography>
-                Nulla facilisi. Phasellus sollicitudin nulla et quam mattis
-                feugiat. Aliquam eget maximus est, id dignissim quam.
-              </Typography>
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
-          <ExpansionPanel
-            expanded={expanded === 'panel2'}
-            onChange={this.handleChange('panel2')}
-          >
-            <ExpansionPanelSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel2bh-content"
-              id="panel2bh-header"
-            >
-              <Typography className={classes.heading}>Users</Typography>
-              <Typography className={classes.secondaryHeading}>
-                You are currently not an owner
-              </Typography>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-              <Typography>
-                Donec placerat, lectus sed mattis semper, neque lectus feugiat
-                lectus, varius pulvinar diam eros in elit. Pellentesque
-                convallis laoreet laoreet.
-              </Typography>
-            </ExpansionPanelDetails>
-          </ExpansionPanel>
-        </div>
-      </div>
-    )
-  }
+				<div className={classes.root}>
+					<ExpansionPanel
+						expanded={expanded === 'panel1'}
+						onChange={this.handleChange('panel1')}
+					>
+						<ExpansionPanelSummary
+							expandIcon={<ExpandMoreIcon />}
+							aria-controls='panel1bh-content'
+							id='panel1bh-header'
+						>
+							<Typography className={classes.heading}>
+								General settings
+							</Typography>
+							<Typography className={classes.secondaryHeading}>
+								I am an expansion panel
+							</Typography>
+						</ExpansionPanelSummary>
+						<ExpansionPanelDetails>
+							<Typography>
+								Nulla facilisi. Phasellus sollicitudin nulla et quam mattis
+								feugiat. Aliquam eget maximus est, id dignissim quam.
+							</Typography>
+						</ExpansionPanelDetails>
+					</ExpansionPanel>
+					<ExpansionPanel
+						expanded={expanded === 'panel2'}
+						onChange={this.handleChange('panel2')}
+					>
+						<ExpansionPanelSummary
+							expandIcon={<ExpandMoreIcon />}
+							aria-controls='panel2bh-content'
+							id='panel2bh-header'
+						>
+							<Typography className={classes.heading}>Users</Typography>
+							<Typography className={classes.secondaryHeading}>
+								You are currently not an owner
+							</Typography>
+						</ExpansionPanelSummary>
+						<ExpansionPanelDetails>
+							<Typography>
+								Donec placerat, lectus sed mattis semper, neque lectus feugiat
+								lectus, varius pulvinar diam eros in elit. Pellentesque
+								convallis laoreet laoreet.
+							</Typography>
+						</ExpansionPanelDetails>
+					</ExpansionPanel>
+				</div>
+			</div>
+		)
+	}
 }
 
 export default withStyles(useStyles)(spots)
