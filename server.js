e61aaaf961cc79b254b6cf2b61c72d4e0832e823
NadaBotros <nadalondos@yahoo.com>
2019-12-08 15:09:43 +0200

    heroku tany

--- server.js
+++ server.js
@@ -21,20 +21,28 @@ app.use(cors())
 app.use(loggerMiddleware)
 
 mongoose
-	.connect(mongoURI, {
-		useCreateIndex: true,
-		useUnifiedTopology: true,
-		useNewUrlParser: true
-	})
-	.then(() => console.log('MongoDB Connected...'))
-	// .then(() => mongoose.connection.db.dropDatabase())
-	// .then(() => console.log('Database Dropped'))
-	// .then(() => populate())
-	// .then(() => console.log('Populated DB'))
-	.catch(err => console.log(err))
+  .connect(mongoURI, {
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  // .then(() => mongoose.connection.db.dropDatabase())
+  // .then(() => console.log('Database Dropped'))
+  // .then(() => populate())
+  // .then(() => console.log('Populated DB'))
+  .catch(err => console.log(err))
 
 // Direct to Route Handlers
 app.use('/api/spots', spots)
+
+//Server static assets if in the production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Server up and running on ${port} 👍 .`))
